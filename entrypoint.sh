64bf87a5d51b0a16bd1ebeb3878267dd4123598a
Tim Gurney aka Wolf <wolf@tgwolf.com>
2022-09-12 19:00:48 +0100

    TESTING 1 2 3

--- entrypoint.sh
+++ entrypoint.sh
@@ -18,24 +18,23 @@ if [ -z "${INPUT_GIT_PUSH_USER_EMAIL}" ]; then
     INPUT_GIT_PUSH_USER_EMAIL="github-actions[bot]@users.noreply.github.com"
 fi
 
+# When the runner maps the $GITHUB_WORKSPACE mount, it is owned by the runner
+# user while the created folders are owned by the container user, causing this
+# error. Issue description here: https://github.com/actions/checkout/issues/766
+git config --global --add safe.directory /github/workspace
+git config --global user.name "${INPUT_GIT_PUSH_USER_NAME}"
+git config --global user.email "${INPUT_GIT_PUSH_USER_EMAIL}"
+git fetch --depth=1 origin +refs/tags/*:refs/tags/* || true
+
 gem install caretaker
 
-echo "HELLO WORLD" >> "${OUTPUT_FILE}"
-
-#caretaker --output "${OUTPUT_FILE}"
+caretaker --silent --output "${OUTPUT_FILE}"
 
 cat "${OUTPUT_FILE}"
 
 exit 0
 
 
-# When the runner maps the $GITHUB_WORKSPACE mount, it is owned by the runner
-# user while the created folders are owned by the container user, causing this
-# error. Issue description here: https://github.com/actions/checkout/issues/766
-git config --global --add safe.directory /github/workspace
-
-git config --global user.name "${INPUT_GIT_PUSH_USER_NAME}"
-git config --global user.email "${INPUT_GIT_PUSH_USER_EMAIL}"
 
 git add "${OUTPUT_FILE}"
 git commit -m "(docs) Update myfile"
